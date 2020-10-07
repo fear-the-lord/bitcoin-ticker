feeb6a3e527e1443240d7badc0cd433c2e03ac77
fear-the-lord <souvikghosh199831@gmail.com>
2020-10-07 21:22:35 +0530

    Initial Commit

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,7 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedValue = 'USD';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +43,28 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+                value: selectedValue,
+                items: [
+                  DropdownMenuItem(
+                    child: Text('USD'),
+                    value: 'USD',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('EUR'),
+                    value: 'EUR',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('GBP'),
+                    value: 'GBP',
+                  ),
+                ],
+                // If onChanged is null, then the button remains in a disabled state
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value;
+                  });
+                }),
           ),
         ],
       ),
