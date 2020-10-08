8cb4fa3bb84603b5490b318c752064318608d345
fear-the-lord <souvikghosh199831@gmail.com>
2020-10-08 21:50:35 +0530

    Final Commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 // Adding all the currencies in the list
 const List<String> currenciesList = [
   'AUD',
@@ -30,4 +33,25 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const String URL = 'https://rest.coinapi.io/v1/exchangerate';
+const String APIKEY = '7B3C3AB1-7EA1-4226-9EC3-20441AFD57AE';
+
+class CoinData {
+  CoinData(this.currency, this.crypto);
+  String currency, crypto;
+
+  Future getCoinData() async {
+    String requestURL = '$URL/$crypto/$currency?apikey=$APIKEY';
+    // Make a GET request to the URL and wait for the response
+    http.Response response = await http.get(requestURL);
+    // Check if the request was successful
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var price = decodedData['rate'];
+      return price;
+    } else {
+      print(response.statusCode);
+      print('Problem with GET request');
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,9 @@
 import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+// Show only the Platform Class
+import 'dart:io' show Platform;
+// import 'dart:io' hide Platform hides Platform class and shows rest
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -7,7 +11,11 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  List<DropdownMenuItem> buildDropDownList() {
+  String selectedValue = currenciesList[0];
+  String bitcoinValueBTC = '?';
+  String bitcoinValueETH = '?';
+  String bitcoinValueLTC = '?';
+  DropdownButton<String> getAndroidDropDownButton() {
     List<DropdownMenuItem<String>> dropDownItems = [];
     for (String currency in currenciesList) {
       var item = DropdownMenuItem(
@@ -16,12 +24,82 @@ class _PriceScreenState extends State<PriceScreen> {
       );
       dropDownItems.add(item);
     }
-    return dropDownItems;
+    return DropdownButton<String>(
+      value: selectedValue,
+      items: dropDownItems,
+// If onChanged is null, then the button remains in a disabled state
+      onChanged: (value) {
+        setState(
+          () {
+            selectedValue = value;
+          },
+        );
+      },
+    );
   }
 
-  String selectedValue = currenciesList[0];
+  CupertinoPicker getIOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItems,
+    );
+  }
+
+//  Widget getPicker() {
+//    if (Platform.isIOS) {
+//      return getIOSPicker();
+//    } else {
+//      return getAndroidDropDownButton();
+//    }
+//  }
+
+  void getDataBTC() async {
+    try {
+      var data = await CoinData(selectedValue, 'BTC').getCoinData();
+      setState(() {
+        bitcoinValueBTC = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getDataETH() async {
+    try {
+      var data = await CoinData(selectedValue, 'ETH').getCoinData();
+      setState(() {
+        bitcoinValueETH = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getDataLTC() async {
+    try {
+      var data = await CoinData(selectedValue, 'LTC').getCoinData();
+      setState(() {
+        bitcoinValueLTC = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDataBTC();
+    getDataETH();
+    getDataLTC();
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -41,7 +119,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 ${cryptoList[0]} = $bitcoinValueBTC  $selectedValue',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ${cryptoList[1]} = $bitcoinValueETH  $selectedValue',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ${cryptoList[2]} = $bitcoinValueLTC  $selectedValue',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -56,15 +176,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton<String>(
-                value: selectedValue,
-                items: buildDropDownList(),
-                // If onChanged is null, then the button remains in a disabled state
-                onChanged: (value) {
-                  setState(() {
-                    selectedValue = value;
-                  });
-                }),
+            child: Platform.isIOS ? getIOSPicker() : getAndroidDropDownButton(),
           ),
         ],
       ),
